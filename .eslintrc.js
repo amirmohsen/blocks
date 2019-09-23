0e0266fe6ace6970024f2301ddb64302803a094a
Amir Mohsen Abdolrazaghi <amirmohsen.a@gmail.com>
2019-09-23 08:47:42 +0100

    eslint updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,9 @@
 module.exports = {
-  "parser": "@typescript-eslint/parser",
   "plugins": [
-    "@typescript-eslint",
     "react-hooks"
   ],
-  "parserOptions": {
-    "project": "./tsconfig.json"
-  },
   "extends": [
     "eslint-config-airbnb",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/typescript",
     "plugin:prettier/recommended"
   ],
   "rules": {
@@ -32,11 +25,52 @@ module.exports = {
       }
     ],
     "indent": "off",
-    "@typescript-eslint/indent": ["error", 2],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
   },
   "env": {
     "browser": true
-  }
+  },
+  "overrides": [
+    {
+      "files": ["*.ts", "*.tsx"],
+      "parser": "@typescript-eslint/parser",
+      "plugins": [
+        "@typescript-eslint",
+        "react-hooks"
+      ],
+      "parserOptions": {
+        "project": "./tsconfig.json"
+      },
+      "extends": [
+        "eslint-config-airbnb",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        "plugin:prettier/recommended"
+      ],
+      "rules": {
+        "react/jsx-indent": [
+          "off"
+        ],
+        "react/jsx-indent-props": [
+          "off"
+        ],
+        "react/jsx-filename-extension": [
+          1,
+          {
+            "extensions": [
+              ".js",
+              ".jsx",
+              ".ts",
+              ".tsx"
+            ]
+          }
+        ],
+        "indent": "off",
+        "@typescript-eslint/indent": ["error", 2],
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn"
+      }
+    }
+  ],
 };
